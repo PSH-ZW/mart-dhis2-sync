ba5cfc158a1d1abf967f7b25e419211be36d7a71
Vishnu <vishnu@nuchange.com>
2022-04-29 10:49:06 +0530

    Minor refactoring

--- src/main/java/com/thoughtworks/martdhis2sync/dao/EnrollmentDAO.java
+++ src/main/java/com/thoughtworks/martdhis2sync/dao/EnrollmentDAO.java
@@ -52,15 +52,14 @@ public class EnrollmentDAO {
         // patient creation date as enrollment date.
         String enrollmentDateSql = "select date_enrolled from program_enrolment where patient_id = ? order by date_enrolled limit 1";
         String patientCreatedDateSql = "select date_created from patient where patient_id = ?";
-        String oldestEnrolmentDate;
         try{
             return jdbcTemplate.queryForObject(enrollmentDateSql, String.class, patientId);
         } catch (DataAccessException e) {
             logger.error("Could not get enrollment date for patient with id {}, getting date_created for patient", patientId);
         }
         try{
-            oldestEnrolmentDate = jdbcTemplate.queryForObject(patientCreatedDateSql, String.class, patientId);
-            return getFormattedDateString(oldestEnrolmentDate, DATEFORMAT_WITH_24HR_TIME, DATEFORMAT_WITHOUT_TIME);
+            String patientCreateDate = jdbcTemplate.queryForObject(patientCreatedDateSql, String.class, patientId);
+            return getFormattedDateString(patientCreateDate, DATEFORMAT_WITH_24HR_TIME, DATEFORMAT_WITHOUT_TIME);
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
