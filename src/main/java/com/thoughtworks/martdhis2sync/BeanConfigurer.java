7a025a3a00df64634e836b5df516726f8fcb40c2
Vishnu <vishnu@nuchange.com>
2022-05-20 12:22:40 +0530

    Use custom RestTemplate to prevent sync hanging due to network timeout

--- src/main/java/com/thoughtworks/martdhis2sync/BeanConfigurer.java
+++ src/main/java/com/thoughtworks/martdhis2sync/BeanConfigurer.java
@@ -1,14 +1,152 @@
 package com.thoughtworks.martdhis2sync;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.feed.AtomFeedHttpMessageConverter;
+import org.springframework.http.converter.feed.RssChannelHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class BeanConfigurer {
+
+    private static final int TIMEOUT = 5 * 60 * 1000;
+    private static final boolean JAXB_2_PRESENT = ClassUtils.isPresent("javax.xml.bind.Binder", RestTemplate.class.getClassLoader());
+    private static final boolean JACKSON_2_PRESENT = ClassUtils.isPresent("com.fasterxml.jackson.databind.ObjectMapper", RestTemplate.class.getClassLoader()) && ClassUtils.isPresent("com.fasterxml.jackson.core.JsonGenerator", RestTemplate.class.getClassLoader());
+    private static final boolean JACKSON_2_XML_PRESENT = ClassUtils.isPresent("com.fasterxml.jackson.dataformat.xml.XmlMapper", RestTemplate.class.getClassLoader());
+    private static final boolean GSON_PRESENT = ClassUtils.isPresent("com.google.gson.Gson", RestTemplate.class.getClassLoader());
+    private static final boolean ROME_PRESENT = ClassUtils.isPresent("com.rometools.rome.feed.WireFeed", RestTemplate.class.getClassLoader());
+
+
+    public static List<HttpMessageConverter<?>> getStandardMessageConvertors() {
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        messageConverters.add(new ByteArrayHttpMessageConverter());
+        messageConverters.add(new StringHttpMessageConverter());
+        messageConverters.add(new ResourceHttpMessageConverter());
+        messageConverters.add(new SourceHttpMessageConverter());
+        messageConverters.add(new AllEncompassingFormHttpMessageConverter());
+        if (ROME_PRESENT) {
+            messageConverters.add(new AtomFeedHttpMessageConverter());
+            messageConverters.add(new RssChannelHttpMessageConverter());
+        }
+        if (JACKSON_2_XML_PRESENT) {
+            messageConverters.add(new MappingJackson2XmlHttpMessageConverter());
+        } else if (JAXB_2_PRESENT) {
+            Jaxb2RootElementHttpMessageConverter e = new Jaxb2RootElementHttpMessageConverter();
+            messageConverters.add(e);
+        }
+        if (JACKSON_2_PRESENT) {
+            messageConverters.add(new MappingJackson2HttpMessageConverter());
+        } else if (GSON_PRESENT) {
+            messageConverters.add(new GsonHttpMessageConverter());
+        }
+        return messageConverters;
+    }
+
+
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplate(HttpClient httpClient) {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        requestFactory.setReadTimeout(TIMEOUT);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.setMessageConverters(getStandardMessageConvertors());
+
+        return restTemplate;
+
+    }
+
+    @Bean
+    public CloseableHttpClient httpClient(PoolingHttpClientConnectionManager poolingHttpClientConnectionManager, RequestConfig requestConfig) {
+        return HttpClientBuilder
+                .create()
+                .setConnectionManager(poolingHttpClientConnectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+    }
+
+    @Bean
+    public PoolingHttpClientConnectionManager httpClientConnectionManager() {
+
+        // Get the poolMaxTotal value from our application[-?].yml or default to 10 if not explicitly set
+
+        // use the TrustSelfSignedStrategy to allow Self Signed Certificates
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContextBuilder
+                    .create()
+                    .loadTrustMaterial(new TrustSelfSignedStrategy())
+                    .build();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        // we can optionally disable hostname verification.
+        // if you don't want to further weaken the security, you don't have to include this.
+        HostnameVerifier allowAllHosts = new NoopHostnameVerifier();
+
+        // create an SSL Socket Factory to use the SSLContext with the trust self signed certificate strategy
+        // and allow all hosts verifier.
+        assert sslContext != null;
+        SSLConnectionSocketFactory connectionFactory = new SSLConnectionSocketFactory(sslContext, allowAllHosts);
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+                .<ConnectionSocketFactory>create().register("https", connectionFactory)
+                .register("http", new PlainConnectionSocketFactory())
+                .build();
+        // finally create the HttpClient using HttpClient factory methods and assign the ssl socket factory
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(
+                socketFactoryRegistry);
+        connectionManager.setMaxTotal(20);
+        return connectionManager;
+    }
+
+    @Bean
+    public RequestConfig requestConfig() {
+        return RequestConfig.custom()
+                .setConnectionRequestTimeout(TIMEOUT)
+                .setConnectTimeout(TIMEOUT)
+                .setSocketTimeout(TIMEOUT)
+                .build();
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(30);
+        return threadPoolTaskScheduler;
     }
 }
--- src/main/java/com/thoughtworks/martdhis2sync/MartDhis2SyncApplication.java
+++ src/main/java/com/thoughtworks/martdhis2sync/MartDhis2SyncApplication.java
@@ -3,9 +3,7 @@ package com.thoughtworks.martdhis2sync;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -14,12 +12,5 @@ public class MartDhis2SyncApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MartDhis2SyncApplication.class, args);
-    }
-
-    @Bean
-    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
-        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-        threadPoolTaskScheduler.setPoolSize(30);
-        return threadPoolTaskScheduler;
     }
 }
